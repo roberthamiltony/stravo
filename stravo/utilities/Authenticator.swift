2d71870fd54d417f12f083ffd44fa86502421d63
Robert Hamilton <rhamilton1415@gmail.com>
2020-02-01 11:24:51 +0000

    feat(Authenticating): add extension and property to allow implementations to use predefined callbackURL structure

--- stravo/utilities/Authenticator.swift
+++ stravo/utilities/Authenticator.swift
@@ -13,7 +13,17 @@ protocol Authenticating {
     /// Whether the object considers itself authenticated
     var authenticated: Bool { get }
     
+    /// The path to append to the callback URL
+    var callbackURLPath: String { get }
+    
     /// A method to attempt authentication
     /// - Parameter completion: An optional completion handler, called with the result of the authentication attempt
     func authenticate(completion: ((Bool) -> Void)?)
+}
+
+extension Authenticating {
+    /// The URL each Authenticating client instance should provide the Authenticating server
+    var callbackURL: String {
+        return "stravo://oauthhandler/" + callbackURLPath
+    }
 }
